91c48dd0b38340364f7b1c6f3011dd15a6b88acf
Kosyo Nikolov <kosyo.nikolov@protonmail.com>
2021-02-08 20:47:10 +0200

    add read from file, log rewards to file

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from environment import Environment
 import numpy as np
 import sys
 import torch
+import os
 from agent import Agent
 
 # 10 vectors in polar coordinates
@@ -17,7 +18,7 @@ CRITIC_L2 = 256
 LR_ACTOR  = 0.000025
 LR_CRITIC = 0.00025
 
-REPLAY_MEMORY_SIZE = 2 * 3600 * 30
+REPLAY_MEMORY_SIZE = 4 * 3600 * 30
 BATCH_SIZE = 64
 
 MAX_EPISODES = 20000
@@ -33,6 +34,16 @@ env = Environment(track, initial_velocity=15)
 agent = Agent(N_STATES, N_ACTIONS, LR_ACTOR, LR_CRITIC, 0.001, 0.99, REPLAY_MEMORY_SIZE, \
              ACTOR_L1, ACTOR_L2, CRITIC_L1, CRITIC_L2, BATCH_SIZE)
 
+if len(sys.argv) == 6:
+    fn_actor = sys.argv[2]
+    fn_critic = sys.argv[3]
+    fn_target_actor = sys.argv[4]
+    fn_target_critic = sys.argv[5]
+
+    agent.load_models(fn_actor, fn_target_actor, fn_critic, fn_target_critic)
+
+
+rewards_log = open("dump/rewards.txt", "w")
 
 for i in range(MAX_EPISODES):
 
@@ -70,6 +81,8 @@ for i in range(MAX_EPISODES):
         
 
     print("ep {0}, reward = {1:.2f}".format(i, total_reward))
+    rewards_log.write("{0:.2f}\n".format(total_reward))
+    rewards_log.flush()
 
     if should_dump:
         agent.save_models()
